919c34a8cbd2ccfb7083410804a3be7b2742b77d
Shun <ter0123540@gmail.com>
2022-04-23 01:17:05 +0800

    finish basic structure and style

---
+++ app.js
@@ -0,0 +1,18 @@
+// sound effect
+let effect = 'break'
+let audio = document.querySelector(`audio[data-key="${effect}"]`);
+// audio.play();
+
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach(icon => {
+    icon.addEventListener('click', (e) => {
+        for (child of e.target.parentNode.children) {
+            child.classList.remove('click')
+        }
+        e.target.classList.add('click');
+        effect = e.target.dataset.key;
+        console.log(effect);
+    })
+})
+
